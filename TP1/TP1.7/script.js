311a81c912c65594a6bbe5a61110e89acbf4d39d
Gabriela Oinhos <gabiih.dev@gmail.com>
2024-07-16 15:25:58 -0300

    Número aleatório e operações

---
+++ TP1/TP1.7/script.js
@@ -0,0 +1,15 @@
+let primeiroNumero = Number(prompt("Digite um número:"))
+let segundoNumero = Number(prompt("Digite outro número:"))
+
+//
+let resultadoAdicao = primeiroNumero + segundoNumero
+console.log(resultadoAdicao)
+
+let resultadoSubtracao = primeiroNumero - segundoNumero
+console.log(resultadoSubtracao)
+
+let resultadoMultiplicacao = primeiroNumero * segundoNumero
+console.log(resultadoMultiplicacao)
+
+let resultadoDivisao = primeiroNumero / segundoNumero
+console.log(resultadoDivisao)                         
