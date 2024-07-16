c044f49ccbfd13d1691a9f4cbe8b33dfb24e1ac5
Gabriela Oinhos <gabiih.dev@gmail.com>
2024-07-16 16:04:51 -0300

    Exibição de mensagens e cálculo de energia cinética

---
+++ TP1/TP1.9/script.js
@@ -0,0 +1,5 @@
+let massa = parseFloat(prompt("Digite a massa:"))
+let velocidade = parseFloat(prompt("Digite a velocidade:"))
+
+let energiaCinetica = (massa * Math.pow(velocidade, 2)) / 2
+alert (energiaCinetica)
