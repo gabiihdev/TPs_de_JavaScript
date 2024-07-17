8a1ce22a6e442a8e5bdb18cc801c97d4822e8641
Gabriela Oinhos <gabiih.dev@gmail.com>
2024-07-17 19:11:03 -0300

    Comentários e atribuições de valores

---
+++ TP1/TP1.14/script.js
@@ -0,0 +1,14 @@
+let a = 10;
+let b = 5;
+
+// b = b * b;
+a = a + 5; //a (10) + 5 = 15
+let resultado = a + b + 5; // 15 + 5 + 5 
+// resultado = resultado + a;
+// resultado *= 1.5;
+resultado -= 5; //25 - 5 = 20
+resultado /= 2; //20 / 2 = 10
+// resultado++;
+
+// O resultado final deve ser 10
+alert(resultado);
