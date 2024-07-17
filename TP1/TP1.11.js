495bdc60fece01e129ae51e11f8d64822136d763
Gabriela Oinhos <gabiih.dev@gmail.com>
2024-07-17 17:59:52 -0300

    Comentários no js

---
+++ TP1/TP1.11.js
@@ -0,0 +1,9 @@
+//O que é programação?
+//Programação é o processo de criação de um conjunto de instruções lógicas/comandos que dizem ao computador como realizar uma tarefa.
+
+//O que é JavaScript?
+//O Javascript é uma linguagem de programação que tem por finalidade dar dinâmica as aplicações web por meio de instruções lógicas.
+
+//Qual a função do Javascript em um navegador Web? (responda usando pelo menos três linhas e os operadores.
+/*A função do Javascript é tornar as páginas web mais dinâmicas, respondendo a interações do usuário. Assim, páginas web que seriam estáticas, se tornam mais interessantes com os elementos executados em Javascript. Outra vantagem de utilizar o Javascript é reduzir o uso do servidor, pois originalmente o Javascript foi pensado para rodar do lado do cliente.
+*/
