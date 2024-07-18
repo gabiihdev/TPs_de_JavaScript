2e66431145118938e375f37997910ff2006ebbc8
Gabriela Oinhos <gabiih.dev@gmail.com>
2024-07-18 17:18:34 -0300

    Conversão de tipo e procedência de operadores

---
+++ TP2/TP2.3/script.js
@@ -0,0 +1,12 @@
+// Não modifique as constantes seguintes
+const a = 1;
+const b = "2";
+const c = 3;
+const d = "4";
+
+//MODIFIQUE AS LINHAS ABAIXO
+
+console.log(typeof a == "number");
+console.log( c * d == 12);
+console.log( b + d == "24");
+console.log(typeof b == "string");
